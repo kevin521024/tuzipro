b42d5ab8578e8388712c5fd05146b7cff11043c4
kevin521024 <kevin521024@gmail.com>
2013-11-11 22:55:40 -0800

    init 1.0.0_common

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tuzipro::Application.config.secret_token = 'd9c5ff322bafc703d49a0618852e211dd4fd6ef58431cf622aaca300f4c5716bc66ad7c8ddea007ec7d34125eb897695bb04b64c2aa81456ce5396720187c0dd'
